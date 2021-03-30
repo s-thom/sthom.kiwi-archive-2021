55b3e83910779319870900162f852cdabe6f66ac
Stuart Thomson <me@sthom.kiwi>
2021-03-30 19:13:59 +1300

    Fix initial errors

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,13 +1,16 @@
+import { Suspense } from 'react';
 import Router from '../components/Router';
-import './App.css';
 import GlobalStyles from './GlobalStyles';
 import Providers from './Providers';
+import GlobalLoading from '../components/GlobalLoading';
 
 export default function App() {
   return (
     <Providers>
       <GlobalStyles />
-      <Router />
+      <Suspense fallback={<GlobalLoading />}>
+        <Router />
+      </Suspense>
     </Providers>
   );
 }
