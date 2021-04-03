0dbece2bcf4cda763fe572c0149f4fad95ef88b4
Stuart Thomson <me@sthom.kiwi>
2021-04-03 14:03:28 +1300

    Minor change

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -25,7 +25,15 @@ const ProfilePhoto = styled.img`
 `;
 
 const Name = styled.h1`
+  font-size: 3em;
   font-weight: 400;
+  margin-bottom: 0;
+`;
+
+const JobLine = styled.p`
+  font-size: 1em;
+  font-weight: 400;
+  margin-top: 0;
 `;
 
 const Icon = styled.svg`
@@ -46,17 +54,26 @@ export default function Home() {
     <VerticalContainer style={animProps}>
       <ProfilePhoto src={portraitSrc} />
       <Name>Stuart Thomson</Name>
+      <JobLine>Software Developer | Human Being</JobLine>
       <HorizontalContainer>
-        <a href="mailto:me@sthom.kiwi" aria-label="Email">
+        <a href="https://write.sthom.kiwi/sthom/" aria-label="Blog">
           <Icon viewBox="0 0 24 24">
-            <title>Email</title>
-            <path d="M22 4H2v16h20V4zm-2 4l-8 5-8-5V6l8 5 8-5v2z" />
+            <title>Blog</title>
+            <path d="M15.54 3.5l4.96 4.97-1.43 1.41-4.95-4.95 1.42-1.43M3.5 19.78l6.5-6.47c-.1-.31-.03-.7.23-.96.39-.39 1.03-.39 1.42 0 .39.4.39 1.03 0 1.42-.26.26-.65.33-.96.23l-6.47 6.5 10.61-3.55 3.53-6.36-4.94-4.95-6.37 3.53L3.5 19.78z" />
           </Icon>
         </a>
         <a href="https://github.com/s-thom" aria-label="GitHub">
           <Icon viewBox="0 0 24 24">
             <title>GitHub</title>
             <path d="M12 2A10 10 0 0 0 2 12a10 10 0 0 0 6.8 9.5c.5 0 .7-.2.7-.5v-1.7C6.7 20 6.1 18 6.1 18c-.4-1.2-1-1.5-1-1.5-1-.6 0-.6 0-.6 1 0 1.5 1 1.5 1 .9 1.6 2.4 1.1 3 .9 0-.7.3-1.1.6-1.4-2.2-.2-4.6-1-4.6-4.9 0-1.1.4-2 1-2.7 0-.3-.4-1.3.2-2.7 0 0 .8-.2 2.7 1a9.4 9.4 0 0 1 5 0c2-1.2 2.8-1 2.8-1 .5 1.4.1 2.4 0 2.7.7.7 1 1.6 1 2.7 0 3.8-2.3 4.7-4.5 5 .4.2.7.8.7 1.8V21c0 .3.2.6.7.5 4-1.3 6.8-5 6.8-9.5A10 10 0 0 0 12 2z" />
+          </Icon>
+        </a>
+      </HorizontalContainer>
+      <HorizontalContainer>
+        <a href="mailto:me@sthom.kiwi" aria-label="Email">
+          <Icon viewBox="0 0 24 24">
+            <title>Email</title>
+            <path d="M22 4H2v16h20V4zm-2 4l-8 5-8-5V6l8 5 8-5v2z" />
           </Icon>
         </a>
         <a href="https://www.linkedin.com/in/s-thom/" aria-label="LinkedIn">
