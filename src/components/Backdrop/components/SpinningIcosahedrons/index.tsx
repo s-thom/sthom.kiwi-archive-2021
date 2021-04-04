f87966043cb3db493fd8a614667add2bbcb6250f
Stuart Thomson <me@sthom.kiwi>
2021-04-04 17:06:47 +1200

    Randomly generate icosohedrons

--- src/components/Backdrop/components/SpinningIcosahedrons/index.tsx
+++ src/components/Backdrop/components/SpinningIcosahedrons/index.tsx
@@ -1,19 +1,37 @@
 import { OrthographicCamera } from '@react-three/drei';
 import { useMemo } from 'react';
+import seedrandom from 'seedrandom';
 import Ico, { IcoProps } from './Ico';
+import useWindowSize from '../../../../hooks/useWindowSize';
 
+/**
+ * Pixels per THREE unit
+ */
 const ZOOM = 40;
+/**
+ * Number of items to have per area unit
+ */
+const DENSITY = 0.000005;
+const RANDOM_SEED = 'stuart';
 
 export default function SpinningIcosahedrons() {
+  const windowSize = useWindowSize();
+
   const icosahedrons = useMemo<IcoProps[]>(() => {
-    return [
-      { position: [-0.5, -2.5, 0], radius: 1 },
-      { position: [-2, 2, 0], radius: 0.25, reverse: true },
-      { position: [2, -1, 0], radius: 0.75, reverse: true },
-      { position: [1.25, 2, 0], radius: 0.75 },
-      { position: [-10, 1.5, -2], radius: 2 },
-    ];
-  }, []);
+    const random = seedrandom(RANDOM_SEED);
+
+    const scaledWidth = windowSize.width / ZOOM;
+    const scaledHeight = windowSize.height / ZOOM;
+
+    const area = windowSize.width * windowSize.height;
+    const numIcos = Math.floor(area * DENSITY);
+
+    return [...Array(numIcos)].map(() => ({
+      position: [random() * scaledWidth - scaledWidth / 2, random() * scaledHeight - scaledHeight / 2, 0],
+      radius: random() ** 2 * 3.5 + 0.25,
+      reverse: random() > 0.5,
+    }));
+  }, [windowSize.height, windowSize.width]);
 
   return (
     <>
