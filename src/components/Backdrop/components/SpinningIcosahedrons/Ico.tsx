bca54efa6a1293dbd0f28c42c76f795064461e73
Stuart Thomson <me@sthom.kiwi>
2021-04-04 16:46:37 +1200

    Make icosohedrons data driven

--- src/components/Backdrop/components/SpinningIcosahedrons/Ico.tsx
+++ src/components/Backdrop/components/SpinningIcosahedrons/Ico.tsx
@@ -1,27 +1,32 @@
 import { MeshProps, useFrame } from '@react-three/fiber';
-import { useRef } from 'react';
+import { useMemo, useRef } from 'react';
 import { useTheme } from 'styled-components';
-import { IcosahedronBufferGeometry } from 'three';
-
-const icosahedronGeometry = new IcosahedronBufferGeometry(1, 0);
 
 export interface IcoProps extends MeshProps {
-  radius: number;
-  rotationSpeed: number;
+  radius?: number;
+  reverse?: boolean;
+  rotationSpeed?: number;
 }
 
-export default function Ico({ radius, rotationSpeed, ...meshProps }: IcoProps) {
+export default function Ico({ radius = 1, rotationSpeed, reverse, ...meshProps }: IcoProps) {
   const theme = useTheme();
+
+  const rotation = useMemo(() => rotationSpeed ?? (reverse ? -1 : 1) / (radius ** 1.5 * 300), [
+    radius,
+    reverse,
+    rotationSpeed,
+  ]);
 
   const mesh = useRef<any>();
   useFrame(() => {
     if (mesh.current) {
-      mesh.current.rotation.y += rotationSpeed;
+      mesh.current.rotation.y += rotation;
     }
   });
 
   return (
-    <mesh ref={mesh} geometry={icosahedronGeometry} {...meshProps}>
+    <mesh ref={mesh} {...meshProps}>
+      <icosahedronBufferGeometry args={[radius, 0]} />
       <meshStandardMaterial color={theme.colors.background} />
     </mesh>
   );
--- src/components/Backdrop/components/SpinningIcosahedrons/index.tsx
+++ src/components/Backdrop/components/SpinningIcosahedrons/index.tsx
@@ -1,11 +1,32 @@
-import Ico from './Ico';
+import { OrthographicCamera } from '@react-three/drei';
+import { useMemo } from 'react';
+import Ico, { IcoProps } from './Ico';
+
+const ZOOM = 40;
 
 export default function SpinningIcosahedrons() {
+  const icosahedrons = useMemo<IcoProps[]>(() => {
+    return [
+      { position: [-0.5, -2.5, 0], radius: 1 },
+      { position: [-2, 2, 0], radius: 0.25, reverse: true },
+      { position: [2, -1, 0], radius: 0.75, reverse: true },
+      { position: [1.25, 2, 0], radius: 0.75 },
+      { position: [-10, 1.5, -2], radius: 2 },
+    ];
+  }, []);
+
   return (
     <>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Ico position={[0, 0, 0]} radius={1} rotationSpeed={-0.0005} />
+      <OrthographicCamera makeDefault position={[0, 0, 10]} zoom={ZOOM}>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <group position={[0, 0, -10]}>
+          {icosahedrons.map((props, index) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <Ico key={index} {...props} />
+          ))}
+        </group>
+      </OrthographicCamera>
     </>
   );
 }
