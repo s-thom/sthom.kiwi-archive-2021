dfc7ee6f7f60a6c760a4031a65caa835a3e81b94
Stuart Thomson <me@sthom.kiwi>
2021-04-13 21:15:25 +1200

    Small cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router';
-import Container from '../src/App/Container';
-import GlobalStyles from '../src/App/GlobalStyles';
+import { NextWebVitalsMetric } from 'next/dist/next-server/lib/utils';
+import Container from '../src/app/Container';
+import GlobalStyles from '../src/app/GlobalStyles';
 import Theme from '../src/components/Theme';
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  // eslint-disable-next-line no-console
+  console.debug(metric);
+}
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
