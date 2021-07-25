3dbc2471c2e11a580dc96f1a347a54d62407e266
Stuart Thomson <me@sthom.kiwi>
2021-07-25 13:16:05 +1200

    Change to light mode

--- src/components/Theme/index.tsx
+++ src/components/Theme/index.tsx
@@ -1,16 +1,25 @@
 import { PropsWithChildren, useMemo } from 'react';
 import { DefaultTheme, ThemeProvider } from 'styled-components';
 
+const LIGHT_MODE_COLOURS: DefaultTheme['colors'] = {
+  primary: '#4273bd',
+  background: '#e4e4e4',
+  text: '#0D1117',
+  warning: '#ce8c11',
+  error: '#972929',
+};
+// const DARK_MODE_COLOURS: DefaultTheme['colors'] = {
+//   primary: '#002355',
+//   background: '#0D1117',
+//   text: '#F0F0F0',
+//   warning: '#ffdd9e',
+//   error: '#ff8080',
+// };
+
 export default function Theme({ children }: PropsWithChildren<{}>) {
   const theme = useMemo<DefaultTheme>(
     () => ({
-      colors: {
-        primary: '#002355',
-        background: '#0D1117',
-        text: '#F0F0F0',
-        warning: '#ffdd9e',
-        error: '#ff8080',
-      },
+      colors: LIGHT_MODE_COLOURS,
       mediaQueries: {
         blog: {
           smallMobile: `min-width: 0px`,
