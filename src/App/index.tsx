63308d05f783349102ff2c83b41484c1412c75f2
Stuart Thomson <me@sthom.kiwi>
2021-07-25 17:12:10 +1200

    Change body typeface to Inter

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,46 +1,10 @@
-import '@fontsource/jetbrains-mono';
-import '@fontsource/jost';
-import '@fontsource/open-sans';
 import { DefaultSeo } from 'next-seo';
 import { DefaultSeoProps, OpenGraph } from 'next-seo/lib/types';
 import { PropsWithChildren, useMemo } from 'react';
-import styled, { createGlobalStyle } from 'styled-components';
+import styled from 'styled-components';
 import Theme from '../components/Theme';
 import og1200x900 from './og-1200-900.png';
 import og800x600 from './og-800-600.png';
-
-const GlobalStyles = createGlobalStyle`
-body {
-  margin: 0;
-  font-family: 'Open Sans', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  font-size: 1.2em;
-}
-
-code {
-  font-family: 'JetBrains Mono', source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
-}
-
-h1,h2,h3,h4,h5,h6 {
-  font-family: Jost, -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
-  font-weight: 400;
-}
-
-@keyframes FadeIn {
-  0% {
-    opacity: 0;
-  }
-
-  100% {
-    opacity: 1;
-  }
-}
-`;
 
 const Container = styled.div`
   max-width: 100vw;
@@ -53,63 +17,60 @@ const Container = styled.div`
 
 export default function App({ children }: PropsWithChildren<{}>) {
   return (
-    <>
-      <Theme>
-        <GlobalStyles />
-        <DefaultSeo
-          defaultTitle="Stuart Thomson"
-          description="Software Developer | Human Being"
-          additionalMetaTags={useMemo<DefaultSeoProps['additionalMetaTags']>(
-            () => [
-              { name: 'viewport', content: 'initial-scale=1.0, width=device-width' },
-              { name: 'theme-color', content: '#0D1117' },
-              { name: 'msapplication-TileColor', content: '#0D1117' },
-              { name: 'msapplication-TileImage', content: '/icons/ms-icon-144x144.png' },
+    <Theme>
+      <DefaultSeo
+        defaultTitle="Stuart Thomson"
+        description="Software Developer | Human Being"
+        additionalMetaTags={useMemo<DefaultSeoProps['additionalMetaTags']>(
+          () => [
+            { name: 'viewport', content: 'initial-scale=1.0, width=device-width' },
+            { name: 'theme-color', content: '#0D1117' },
+            { name: 'msapplication-TileColor', content: '#0D1117' },
+            { name: 'msapplication-TileImage', content: '/icons/ms-icon-144x144.png' },
+          ],
+          [],
+        )}
+        additionalLinkTags={useMemo<DefaultSeoProps['additionalLinkTags']>(
+          () => [
+            { rel: 'author', type: 'text/plain', href: '/humans.txt' },
+            { rel: 'icon', type: 'image/x-icon', sizes: '16x16', href: '/favicon.ico' },
+            { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/icons/android-icon-192x192.png' },
+            { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/icons/favicon-96x96.png' },
+            { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/icons/favicon-32x32.png' },
+            { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/icons/favicon-16x16.png' },
+            { rel: 'apple-touch-icon', sizes: '57x57', href: '/apple-icon-57x57.png' },
+            { rel: 'apple-touch-icon', sizes: '60x60', href: '/apple-icon-60x60.png' },
+            { rel: 'apple-touch-icon', sizes: '72x72', href: '/apple-icon-72x72.png' },
+            { rel: 'apple-touch-icon', sizes: '76x76', href: '/apple-icon-76x76.png' },
+            { rel: 'apple-touch-icon', sizes: '114x114', href: '/apple-icon-114x114.png' },
+            { rel: 'apple-touch-icon', sizes: '120x120', href: '/apple-icon-120x120.png' },
+            { rel: 'apple-touch-icon', sizes: '144x144', href: '/apple-icon-144x144.png' },
+            { rel: 'apple-touch-icon', sizes: '152x152', href: '/apple-icon-152x152.png' },
+            { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-icon-180x180.png' },
+            { rel: 'manifest', href: '/manifest.json' },
+          ],
+          [],
+        )}
+        openGraph={useMemo<OpenGraph>(
+          () => ({
+            type: 'website',
+            site_name: 'Stuart Thomson',
+            title: 'Stuart Thomson',
+            description: 'Software Developer | Human Being',
+            profile: {
+              firstName: 'Stuart',
+              lastName: 'Thomson',
+              username: 'sthom',
+            },
+            images: [
+              { url: og800x600.src, alt: 'Stuart Thomson', height: 600, width: 800 },
+              { url: og1200x900.src, alt: 'Stuart Thomson', height: 900, width: 1200 },
             ],
-            [],
-          )}
-          additionalLinkTags={useMemo<DefaultSeoProps['additionalLinkTags']>(
-            () => [
-              { rel: 'author', type: 'text/plain', href: '/humans.txt' },
-              { rel: 'icon', type: 'image/x-icon', sizes: '16x16', href: '/favicon.ico' },
-              { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/icons/android-icon-192x192.png' },
-              { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/icons/favicon-96x96.png' },
-              { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/icons/favicon-32x32.png' },
-              { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/icons/favicon-16x16.png' },
-              { rel: 'apple-touch-icon', sizes: '57x57', href: '/apple-icon-57x57.png' },
-              { rel: 'apple-touch-icon', sizes: '60x60', href: '/apple-icon-60x60.png' },
-              { rel: 'apple-touch-icon', sizes: '72x72', href: '/apple-icon-72x72.png' },
-              { rel: 'apple-touch-icon', sizes: '76x76', href: '/apple-icon-76x76.png' },
-              { rel: 'apple-touch-icon', sizes: '114x114', href: '/apple-icon-114x114.png' },
-              { rel: 'apple-touch-icon', sizes: '120x120', href: '/apple-icon-120x120.png' },
-              { rel: 'apple-touch-icon', sizes: '144x144', href: '/apple-icon-144x144.png' },
-              { rel: 'apple-touch-icon', sizes: '152x152', href: '/apple-icon-152x152.png' },
-              { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-icon-180x180.png' },
-              { rel: 'manifest', href: '/manifest.json' },
-            ],
-            [],
-          )}
-          openGraph={useMemo<OpenGraph>(
-            () => ({
-              type: 'website',
-              site_name: 'Stuart Thomson',
-              title: 'Stuart Thomson',
-              description: 'Software Developer | Human Being',
-              profile: {
-                firstName: 'Stuart',
-                lastName: 'Thomson',
-                username: 'sthom',
-              },
-              images: [
-                { url: og800x600.src, alt: 'Stuart Thomson', height: 600, width: 800 },
-                { url: og1200x900.src, alt: 'Stuart Thomson', height: 900, width: 1200 },
-              ],
-            }),
-            [],
-          )}
-        />
-        <Container>{children}</Container>
-      </Theme>
-    </>
+          }),
+          [],
+        )}
+      />
+      <Container>{children}</Container>
+    </Theme>
   );
 }
--- src/components/Theme/index.tsx
+++ src/components/Theme/index.tsx
@@ -1,5 +1,8 @@
+import '@fontsource/inter';
+import '@fontsource/jetbrains-mono';
+import '@fontsource/jost';
 import { PropsWithChildren, useMemo } from 'react';
-import { DefaultTheme, ThemeProvider } from 'styled-components';
+import { createGlobalStyle, DefaultTheme, ThemeProvider } from 'styled-components';
 
 const LIGHT_MODE_COLOURS: DefaultTheme['colors'] = {
   primary: '#4273bd',
@@ -15,6 +18,43 @@ const LIGHT_MODE_COLOURS: DefaultTheme['colors'] = {
 //   warning: '#ffdd9e',
 //   error: '#ff8080',
 // };
+
+const GlobalStyles = createGlobalStyle`
+body {
+  margin: 0;
+  font-family: 'InterVariable', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-size: 1.2em;
+  /* Fontsource doesn't package stylistic sets. If I ever decide to fully self-host the font files, then these
+   * can be enabled again.
+   */
+  /* font-feature-settings: "tnum", "ss02", "cv03", "cv04"; */
+}
+
+code {
+  font-family: 'JetBrains Mono', source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
+}
+
+h1,h2,h3,h4,h5,h6 {
+  font-family: Jost, -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  font-weight: 400;
+}
+
+@keyframes FadeIn {
+  0% {
+    opacity: 0;
+  }
+
+  100% {
+    opacity: 1;
+  }
+}
+`;
 
 export default function Theme({ children }: PropsWithChildren<{}>) {
   const theme = useMemo<DefaultTheme>(
@@ -34,5 +74,10 @@ export default function Theme({ children }: PropsWithChildren<{}>) {
     [],
   );
 
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      {children}
+    </ThemeProvider>
+  );
 }
