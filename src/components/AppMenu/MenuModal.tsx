5c9c83169ae427b9f91dc912f1927df2a796ee86
Stuart Thomson <me@sthom.kiwi>
2021-08-04 17:59:21 +1200

    Add everything to modal

--- src/components/AppMenu/MenuModal.tsx
+++ src/components/AppMenu/MenuModal.tsx
@@ -19,13 +19,50 @@ const ModalShade = styled.div`
 
 const ModalContentContainer = styled.div`
   position: fixed;
+  display: flex;
+  flex-direction: column;
   top: 2em;
-  left: 2em;
+  left: 50%;
+  transform: translateX(-50%);
   width: calc(100vw - 4em);
   height: calc(100vh - 4em);
+  max-width: 20em;
+  max-height: 15em;
   z-index: 11;
 
   background-color: ${({ theme }) => theme.colors.modal};
+  color: ${({ theme }) => theme.colors.text};
+`;
+
+const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 1em;
+  flex-grow: 1;
+`;
+
+const ModalFill = styled.div`
+  flex-grow: 1;
+`;
+
+const CloseIcon = styled.svg`
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 1.5em;
+  height: 1.5em;
+  margin: 0.5em;
+  fill: ${({ theme }) => theme.colors.text};
+  display: block;
+`;
+
+const ModalTitle = styled.h2`
+  text-align: center;
+`;
+
+const Link = styled.a`
+  font-size: 0.8em;
+  color: ${({ theme }) => theme.colors.text};
 `;
 
 export interface MenuModalProps {
@@ -48,17 +85,34 @@ export default function MenuModal({ isOpen, onClose }: MenuModalProps) {
       <ModalWrapper>
         <ModalShade onClick={onClose} />
         <ModalContentContainer>
-          <div>
-            <h2>Settings</h2>
-          </div>
-          <div>
-            <p>Theme</p>
-            <select value={preference} onChange={changeHandler}>
-              <option value="auto">System Auto</option>
-              <option value="light">Light</option>
-              <option value="dark">Dark</option>
-            </select>
-          </div>
+          <CloseIcon role="button" viewBox="0 0 24 24" onClick={onClose}>
+            <title>Close</title>
+            <path d="M0 0h24v24H0V0z" fill="none" />
+            <path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
+          </CloseIcon>
+          <ModalContent>
+            <ModalTitle>Site Preferences</ModalTitle>
+            <ModalFill>
+              <p>
+                Theme:{' '}
+                <select value={preference} onChange={changeHandler}>
+                  <option value="auto">System Auto</option>
+                  <option value="light">Light</option>
+                  <option value="dark">Dark</option>
+                </select>
+              </p>
+            </ModalFill>
+            <p>
+              <Link
+                href="https://github.com/s-thom/sthom.kiwi"
+                aria-label="GitHub"
+                rel="nofollow noopener noreferrer"
+                target="_blank"
+              >
+                Website source code
+              </Link>
+            </p>
+          </ModalContent>
         </ModalContentContainer>
       </ModalWrapper>
     </Fade>
