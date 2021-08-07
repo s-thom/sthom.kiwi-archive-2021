eaa1ad8fbe9f3cd204e04b7f945fae374ce29cb8
Stuart Thomson <me@sthom.kiwi>
2021-08-07 15:48:02 +1200

    Small cleanup of scroll code

--- src/components/AppMenu/index.tsx
+++ src/components/AppMenu/index.tsx
@@ -1,34 +1,34 @@
-import { useEffect, useCallback } from 'react';
-import { usePreviousDistinct, useToggle, useWindowScroll } from 'react-use';
+import { useState, useEffect, useCallback } from 'react';
+import { usePreviousDistinct, useWindowScroll } from 'react-use';
 import MenuButton from './MenuButton';
 import MenuModal from './MenuModal';
 
 export default function AppMenu() {
-  const [isOpen, toggleIsOpen] = useToggle(false);
-  const [shouldShowMenu, toggleShouldShowMenu] = useToggle(true);
+  const [isOpen, setIsOpen] = useState(false);
+  const [shouldShowMenu, setShouldShowMenu] = useState(true);
 
   const onOpen = useCallback(() => {
-    toggleIsOpen(true);
-    toggleShouldShowMenu(false);
-  }, [toggleIsOpen, toggleShouldShowMenu]);
+    setIsOpen(true);
+    setShouldShowMenu(false);
+  }, [setIsOpen, setShouldShowMenu]);
   const onClose = useCallback(() => {
-    toggleIsOpen(false);
-    toggleShouldShowMenu(true);
-  }, [toggleIsOpen, toggleShouldShowMenu]);
+    setIsOpen(false);
+    setShouldShowMenu(true);
+  }, [setIsOpen, setShouldShowMenu]);
 
   const { y: windowScrollY } = useWindowScroll();
-  const previousWindowScrollY = usePreviousDistinct(windowScrollY);
+  const previousWindowScrollY = usePreviousDistinct(windowScrollY) ?? 0;
   useEffect(() => {
-    if (windowScrollY < (previousWindowScrollY ?? 0)) {
-      toggleShouldShowMenu(true);
-    } else if (windowScrollY > (previousWindowScrollY ?? 0)) {
-      toggleShouldShowMenu(false);
+    if (windowScrollY < previousWindowScrollY) {
+      setShouldShowMenu(true);
+    } else if (windowScrollY > previousWindowScrollY) {
+      setShouldShowMenu(false);
     }
-  }, [previousWindowScrollY, toggleShouldShowMenu, windowScrollY]);
+  }, [previousWindowScrollY, setShouldShowMenu, windowScrollY]);
 
   return (
     <>
-      <MenuButton isOpen={!shouldShowMenu} onClick={onOpen} />
+      <MenuButton isVisible={shouldShowMenu} onClick={onOpen} />
       <MenuModal isOpen={isOpen} onClose={onClose} />
     </>
   );
