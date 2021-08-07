71dbb834114cf52fc09e35f2fcc437c527d830d2
Stuart Thomson <me@sthom.kiwi>
2021-08-07 15:44:05 +1200

    Hide/show menu when scrolling down/up

--- src/components/AppMenu/index.tsx
+++ src/components/AppMenu/index.tsx
@@ -1,14 +1,35 @@
-import { useToggle } from 'react-use';
+import { useEffect, useCallback } from 'react';
+import { usePreviousDistinct, useToggle, useWindowScroll } from 'react-use';
 import MenuButton from './MenuButton';
 import MenuModal from './MenuModal';
 
 export default function AppMenu() {
   const [isOpen, toggleIsOpen] = useToggle(false);
+  const [shouldShowMenu, toggleShouldShowMenu] = useToggle(true);
+
+  const onOpen = useCallback(() => {
+    toggleIsOpen(true);
+    toggleShouldShowMenu(false);
+  }, [toggleIsOpen, toggleShouldShowMenu]);
+  const onClose = useCallback(() => {
+    toggleIsOpen(false);
+    toggleShouldShowMenu(true);
+  }, [toggleIsOpen, toggleShouldShowMenu]);
+
+  const { y: windowScrollY } = useWindowScroll();
+  const previousWindowScrollY = usePreviousDistinct(windowScrollY);
+  useEffect(() => {
+    if (windowScrollY < (previousWindowScrollY ?? 0)) {
+      toggleShouldShowMenu(true);
+    } else if (windowScrollY > (previousWindowScrollY ?? 0)) {
+      toggleShouldShowMenu(false);
+    }
+  }, [previousWindowScrollY, toggleShouldShowMenu, windowScrollY]);
 
   return (
     <>
-      <MenuButton isOpen={isOpen} onClick={toggleIsOpen} />
-      <MenuModal isOpen={isOpen} onClose={toggleIsOpen} />
+      <MenuButton isOpen={!shouldShowMenu} onClick={onOpen} />
+      <MenuModal isOpen={isOpen} onClose={onClose} />
     </>
   );
 }
