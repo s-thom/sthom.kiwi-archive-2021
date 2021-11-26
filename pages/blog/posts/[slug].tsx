d22ec146d47d1c89a1c09aa3881e88d48eb45a2a
Stuart Thomson <me@sthom.kiwi>
2021-11-26 17:19:56 +1300

    Change post page to get info from Notion
    Much of this is "inspired" by react-notion-x's Next.js example.

--- pages/blog/posts/[slug].tsx
+++ pages/blog/posts/[slug].tsx
@@ -1,70 +1,57 @@
-import { promises as fs } from 'fs';
 import 'katex/dist/katex.min.css';
 import { GetStaticProps } from 'next';
-import { MDXRemoteSerializeResult } from 'next-mdx-remote';
 import { NextSeo } from 'next-seo';
-import ErrorPage from 'next/error';
 import { useRouter } from 'next/router';
-import path from 'path';
-import styled from 'styled-components';
+import { NotionAPI } from 'notion-client';
+import { ExtendedRecordMap } from 'notion-types';
+import { getAllPagesInSpace, getPageTitle } from 'notion-utils';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'prismjs/themes/prism-tomorrow.css';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'rc-dropdown/assets/index.css';
+import { Collection, CollectionRow, NotionRenderer } from 'react-notion-x';
+import 'react-notion-x/src/styles.css';
 import Layout from '../../../src/components/Layout';
-import Link from '../../../src/components/Link';
-import Tag from '../../../src/components/Tag';
-import { MdxRenderer } from '../../../src/mdx/client';
-import { getMarkdownSource } from '../../../src/mdx/server';
-import { PostMeta } from '../../../src/types/post';
 
-const Warning = styled.div`
-  border: 1px solid ${({ theme }) => theme.colors.warning};
-  color: ${({ theme }) => theme.colors.warning};
-  background-color: ${({ theme }) => `${theme.colors.warning}44`};
-  padding: 0.25em;
-`;
+const isDev = process.env.NODE_ENV === 'development' || !process.env.NODE_ENV;
+const rootNotionSpaceId = process.env.NOTION_SPACE;
+const rootNotionPageId = process.env.NOTION_COLLECTION;
 
-const TagList = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  font-size: 0.8em;
-`;
+const notion = new NotionAPI();
 
 interface PostProps {
-  source: MDXRemoteSerializeResult;
-  frontMatter: PostMeta;
+  recordMap: ExtendedRecordMap;
 }
 
-export default function Post({ source, frontMatter }: PostProps) {
+export default function Post({ recordMap }: PostProps) {
   const router = useRouter();
   const { slug } = router.query;
 
-  if (!source || (!router.isFallback && !frontMatter.title)) {
-    return <ErrorPage statusCode={404} />;
+  if (!recordMap) {
+    return null;
   }
+
+  const title = getPageTitle(recordMap);
 
   return (
     <Layout
       breadcrumbs={[
         { path: '/', name: 'Home' },
         { path: '/blog', name: 'Blog' },
-        { path: `/blog/posts/${slug}`, name: frontMatter.title },
+        { path: `/blog/posts/${slug}`, name: title },
       ]}
-      aside={
-        frontMatter.tags ? (
-          <>
-            <h3>Tags</h3>
-            <TagList>
-              {frontMatter.tags.map((tag) => (
-                <Link key={tag} href={`/blog/tags/${encodeURI(tag)}`}>
-                  <Tag name={tag} />
-                </Link>
-              ))}
-            </TagList>
-          </>
-        ) : undefined
-      }
     >
-      {!frontMatter.published && <Warning>This post is a draft and has not been published yet</Warning>}
-      <NextSeo title={`${frontMatter.title} | Blog | Stuart Thomson`} description={frontMatter.description} />
-      <MdxRenderer {...source} />
+      {/* {!frontMatter.published && <Warning>This post is a draft and has not been published yet</Warning>} */}
+      <NextSeo title={`${title} | Blog | Stuart Thomson`} />
+      <NotionRenderer
+        recordMap={recordMap}
+        fullPage
+        darkMode={false}
+        components={{
+          collection: Collection,
+          collectionRow: CollectionRow,
+        }}
+      />
     </Layout>
   );
 }
@@ -76,27 +63,39 @@ export const getStaticProps: GetStaticProps<PostProps, { slug: string }> = async
     };
   }
 
-  const filePath = path.join(process.cwd(), 'content/posts', `${decodeURI(params.slug)}.mdx`);
-  const fileContents = await fs.readFile(filePath, 'utf8');
+  try {
+    const recordMap = await notion.getPage(params.slug);
 
-  const { frontMatter, source } = await getMarkdownSource(fileContents);
-
-  return {
-    props: {
-      source,
-      frontMatter,
-    },
-  };
+    return {
+      props: {
+        recordMap,
+      },
+      revalidate: 10,
+    };
+  } catch (err) {
+    return {
+      notFound: true,
+    };
+  }
 };
 
 export async function getStaticPaths() {
-  const postsDirectory = path.join(process.cwd(), 'content/posts');
-  const filenames = await fs.readdir(postsDirectory);
+  if (isDev) {
+    return {
+      paths: [],
+      fallback: true,
+    };
+  }
 
-  const paths = filenames
-    .filter((filename) => filename.match(/\.mdx$/))
-    .map((filename) => filename.match(/^(.*)\.mdx$/)![1])
-    .map((slug) => `/blog/posts/${encodeURI(slug)}`);
+  if (!rootNotionPageId) {
+    throw new Error('Root page ID was not specified. Check the "NOTION_COLLECTION" environment variable');
+  }
+
+  const pages = await getAllPagesInSpace(rootNotionPageId, rootNotionSpaceId, notion.getPage.bind(notion), {
+    traverseCollections: false,
+  });
+
+  const paths = Object.keys(pages).map((pageId) => `/blog/posts/${pageId}`);
 
   return {
     paths,
