f671e9115fab3bac6782984845679f44bdd9ff45
Stuart Thomson <me@sthom.kiwi>
2022-01-05 11:04:02 +1300

    Fix post listing page

--- pages/blog/posts/[slug].tsx
+++ pages/blog/posts/[slug].tsx
@@ -1,7 +1,5 @@
-import 'katex/dist/katex.min.css';
 import { GetStaticProps } from 'next';
 import { NextSeo } from 'next-seo';
-import dynamic from 'next/dynamic';
 import ErrorPage from 'next/error';
 import { useRouter } from 'next/router';
 import { NotionAPI } from 'notion-client';
@@ -15,20 +13,11 @@ import {
 } from 'notion-utils';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import 'rc-dropdown/assets/index.css';
-import { Collection, NotionRenderer } from 'react-notion-x';
 import 'react-notion-x/src/styles.css';
 import styled from 'styled-components';
 import Layout from '../../../src/components/Layout';
-import Link from '../../../src/components/Link';
-import { useThemeMode } from '../../../src/hooks/useThemeMode';
+import Notion from '../../../src/components/Notion';
 import { highlightCode } from '../../../src/shiki';
-
-// @ts-ignore
-const Equation = dynamic(() => import('react-notion-x').then((notion) => notion.Equation));
-
-const Modal = dynamic(() => import('react-notion-x').then((notion) => notion.Modal), { ssr: false });
-
-const Code = dynamic(() => import('../../../src/components/Code'));
 
 const isDev = process.env.NODE_ENV === 'development' || !process.env.NODE_ENV;
 /* eslint-disable prefer-destructuring */
@@ -38,56 +27,11 @@ const NOTION_COLLECTION = process.env.NOTION_COLLECTION;
 /* eslint-enable prefer-destructuring */
 const notion = new NotionAPI();
 
-const StyledNotionRenderer = styled(NotionRenderer)`
-  font-family: inherit;
-
-  &.light-mode {
-    --fg-color: ${({ theme }) => theme.colors.text};
-    --fg-color-0: rgba(55, 53, 47, 0.09);
-    --fg-color-1: rgba(55, 53, 47, 0.16);
-    --fg-color-2: rgba(55, 53, 47, 0.4);
-    --fg-color-3: rgba(55, 53, 47, 0.6);
-    --fg-color-4: #000;
-    --fg-color-5: rgba(55, 53, 47, 0.024);
-    --fg-color-6: rgba(55, 53, 47, 0.8);
-    --fg-color-icon: var(--fg-color);
-
-    --bg-color: ${({ theme }) => theme.colors.background};
-    --bg-color-0: rgba(135, 131, 120, 0.15);
-    --bg-color-1: rgb(247, 246, 243);
-    --bg-color-2: rgba(135, 131, 120, 0.15);
-  }
-
-  &.dark-mode {
-    --fg-color: ${({ theme }) => theme.colors.text};
-    --fg-color-0: var(--fg-color);
-    --fg-color-1: var(--fg-color);
-    --fg-color-2: var(--fg-color);
-    --fg-color-3: var(--fg-color);
-    --fg-color-4: var(--fg-color);
-    --fg-color-5: rgba(255, 255, 255, 0.7);
-    --fg-color-6: #fff;
-    --fg-color-icon: #fff;
-
-    --bg-color: ${({ theme }) => theme.colors.background};
-    --bg-color-0: rgb(71, 76, 80);
-    --bg-color-1: rgb(63, 68, 71);
-    --bg-color-2: rgba(135, 131, 120, 0.15);
-  }
-
-  .notion-viewport {
-    position: absolute;
-    width: 100%;
-  }
-
-  .notion-header {
+const BlogPageNotion = styled(Notion)`
+  .notion-title + .notion-collection-row {
     display: none;
   }
 `;
-
-function CollectionRow() {
-  return null;
-}
 
 interface PostProps {
   recordMap: ExtendedRecordMap | null;
@@ -96,7 +40,6 @@ interface PostProps {
 export default function Post({ recordMap }: PostProps) {
   const router = useRouter();
   const { slug } = router.query;
-  const { mode } = useThemeMode();
 
   if (!recordMap) {
     return <ErrorPage statusCode={404} />;
@@ -114,20 +57,7 @@ export default function Post({ recordMap }: PostProps) {
     >
       {/* {!frontMatter.published && <Warning>This post is a draft and has not been published yet</Warning>} */}
       <NextSeo title={`${title} | Blog | Stuart Thomson`} />
-      <StyledNotionRenderer
-        recordMap={recordMap}
-        fullPage
-        darkMode={mode === 'dark'}
-        components={{
-          code: Code,
-          collection: Collection,
-          collectionRow: CollectionRow,
-          pageLink: Link,
-          modal: Modal,
-          equation: Equation,
-        }}
-        mapPageUrl={(id) => `/blog/posts/${getCanonicalPageId(id, recordMap)}`}
-      />
+      <BlogPageNotion recordMap={recordMap} />
     </Layout>
   );
 }
---
+++ src/components/Notion/index.tsx
@@ -0,0 +1,109 @@
+import 'katex/dist/katex.min.css';
+import dynamic from 'next/dynamic';
+import { ExtendedRecordMap } from 'notion-types';
+import { getCanonicalPageId } from 'notion-utils';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'rc-dropdown/assets/index.css';
+import { NotionRenderer } from 'react-notion-x';
+import 'react-notion-x/src/styles.css';
+import styled from 'styled-components';
+import { useThemeMode } from '../../../src/hooks/useThemeMode';
+import Link from '../Link';
+
+// @ts-ignore
+const Equation = dynamic(() => import('react-notion-x').then((notion) => notion.Equation));
+// @ts-ignore
+const Collection = dynamic(() => import('react-notion-x').then((notion) => notion.Collection));
+// @ts-ignore
+const CollectionRow = dynamic(() => import('react-notion-x').then((notion) => notion.CollectionRow));
+const Modal = dynamic(() => import('react-notion-x').then((notion) => notion.Modal), { ssr: false });
+const Code = dynamic(() => import('../../../src/components/Code'));
+
+const StyledNotionRenderer = styled(NotionRenderer)`
+  font-family: inherit;
+
+  &.light-mode {
+    --fg-color: ${({ theme }) => theme.colors.text};
+    --fg-color-0: rgba(55, 53, 47, 0.09);
+    --fg-color-1: rgba(55, 53, 47, 0.16);
+    --fg-color-2: rgba(55, 53, 47, 0.4);
+    --fg-color-3: rgba(55, 53, 47, 0.6);
+    --fg-color-4: #000;
+    --fg-color-5: rgba(55, 53, 47, 0.024);
+    --fg-color-6: rgba(55, 53, 47, 0.8);
+    --fg-color-icon: var(--fg-color);
+
+    --bg-color: ${({ theme }) => theme.colors.background};
+    --bg-color-0: rgba(135, 131, 120, 0.15);
+    --bg-color-1: rgb(247, 246, 243);
+    --bg-color-2: rgba(135, 131, 120, 0.15);
+  }
+
+  &.dark-mode {
+    --fg-color: ${({ theme }) => theme.colors.text};
+    --fg-color-0: var(--fg-color);
+    --fg-color-1: var(--fg-color);
+    --fg-color-2: var(--fg-color);
+    --fg-color-3: var(--fg-color);
+    --fg-color-4: var(--fg-color);
+    --fg-color-5: rgba(255, 255, 255, 0.7);
+    --fg-color-6: #fff;
+    --fg-color-icon: #fff;
+
+    --bg-color: ${({ theme }) => theme.colors.background};
+    --bg-color-0: rgb(71, 76, 80);
+    --bg-color-1: rgb(63, 68, 71);
+    --bg-color-2: rgba(135, 131, 120, 0.15);
+  }
+
+  .notion-page-cover {
+    border-radius: 2em;
+  }
+
+  .notion-page.notion-page-has-cover {
+    padding-top: 56px;
+  }
+
+  .notion-title {
+    font-weight: 400;
+  }
+
+  .notion-viewport {
+    position: absolute;
+    width: 100%;
+  }
+
+  .notion-header {
+    display: none;
+  }
+
+  .notion-gallery {
+    width: 100%;
+  }
+`;
+
+export interface NotionProps {
+  recordMap: ExtendedRecordMap;
+}
+
+export default function Notion({ recordMap, ...rest }: NotionProps) {
+  const { mode } = useThemeMode();
+
+  return (
+    <StyledNotionRenderer
+      recordMap={recordMap}
+      fullPage
+      darkMode={mode === 'dark'}
+      components={{
+        code: Code,
+        collection: Collection,
+        collectionRow: CollectionRow,
+        pageLink: Link,
+        modal: Modal,
+        equation: Equation,
+      }}
+      mapPageUrl={(id) => `/blog/posts/${getCanonicalPageId(id, recordMap)}`}
+      {...rest}
+    />
+  );
+}
