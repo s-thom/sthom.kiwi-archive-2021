c9c7356c0640d1e441819f88c02da43632d752f7
Stuart Thomson <me@sthom.kiwi>
2022-01-05 09:08:08 +1300

    Change layout
    I was not happy with the left sidebar design

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -4,85 +4,28 @@ import Header, { HeaderProps } from './Header';
 
 const Container = styled.div`
   margin: 0 auto;
-  padding: 0 1em;
-  max-width: 30em;
-  min-height: 100vh;
-  height: 100%;
 
-  @media (${({ theme }) => theme.mediaQueries.blog.tablet}) {
-    max-width: 50em;
-  }
-
-  @media (${({ theme }) => theme.mediaQueries.blog.desktop}) {
-    max-width: 60em;
-  }
-
-  @media (${({ theme }) => theme.mediaQueries.blog.largeDesktop}) {
-    max-width: 70em;
-  }
+  width: 90vw;
+  width: clamp(16rem, 93vw, 75rem);
 
   animation: 0.5s ease-in FadeIn;
 `;
 
-const GridContainer = styled.div`
-  display: grid;
-  grid-template-areas:
-    'header'
-    'content'
-    'aside';
-  grid-template-rows: max-content;
-  grid-template-columns: 100%;
-  gap: 0.5em;
-  padding-bottom: 1em;
-
-  @media (${({ theme }) => theme.mediaQueries.blog.tablet}) {
-    padding-top: 1em;
-    grid-template-areas:
-      'header content'
-      'aside content';
-    grid-template-columns: 10em 1fr 1em;
-    gap: 1em;
-  }
-
-  @media (${({ theme }) => theme.mediaQueries.blog.desktop}) {
-    grid-template-columns: 10em 1fr 2em;
-  }
-
-  @media (${({ theme }) => theme.mediaQueries.blog.largeDesktop}) {
-    grid-template-columns: 20em 1fr 5em;
-  }
-`;
-
 const ContentArea = styled.div`
-  grid-area: content;
   font-size: 1.1em;
 
-  @media (${({ theme }) => theme.mediaQueries.blog.tablet}) {
-    padding-top: 1em;
-  }
-`;
-
-const HeaderArea = styled(Header)`
-  grid-area: header;
-`;
-
-const AsideArea = styled.aside`
-  grid-area: aside;
+  margin-top: 0.2em;
 `;
 
 export interface LayoutProps {
   breadcrumbs?: HeaderProps['breadcrumbs'];
-  aside?: ReactNode;
 }
 
-export default function Layout({ breadcrumbs, aside, children }: PropsWithChildren<LayoutProps>) {
+export default function Layout({ breadcrumbs, children }: PropsWithChildren<LayoutProps>) {
   return (
     <Container>
-      <GridContainer>
-        <HeaderArea breadcrumbs={breadcrumbs} />
-        <ContentArea>{children}</ContentArea>
-        {aside && <AsideArea>{aside}</AsideArea>}
-      </GridContainer>
+      <Header breadcrumbs={breadcrumbs} />
+      <ContentArea>{children}</ContentArea>
     </Container>
   );
 }
