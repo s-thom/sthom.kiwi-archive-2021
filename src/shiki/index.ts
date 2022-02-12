586f50335fc558777f4dba0b944d1c2a890572bf
Stuart Thomson <me@sthom.kiwi>
2022-02-12 17:06:31 +1300

    Add additional way to specify code highlighting language

--- src/shiki/index.ts
+++ src/shiki/index.ts
@@ -7,13 +7,24 @@ const highlighterPromise = getHighlighter({ theme: syntaxTheme as unknown as ISh
 
 export async function highlightCode(block: CodeBlock): Promise<string | undefined> {
   const notionLanguage = block.properties?.language?.[0]?.[0];
-  const content = block.properties?.title?.[0]?.[0];
+  const blockContent = block.properties?.title?.[0]?.[0];
 
-  if (notionLanguage && content) {
-    const fallback = notionLanguage.toLowerCase();
-    const language = (languageMap[fallback] ?? fallback) || 'tsx';
+  if (notionLanguage && blockContent) {
+    let codeLanguage = notionLanguage.toLowerCase();
+    let codeString = blockContent;
+    // Try match language from first line of text
+    // Match get a "comment", followed by the keyword "lang"
+    const match = blockContent.match(/^(?:[/#]+\s*lang:?\s*(\S*)\r?\n)([\s\S]*)$/);
+    if (match) {
+      // eslint-disable-next-line prefer-destructuring
+      codeLanguage = match[1];
+      // eslint-disable-next-line prefer-destructuring
+      codeString = match[2];
+    }
+
+    const finalLanguage = (languageMap[codeLanguage] ?? codeLanguage) || 'tsx';
     const highlighter = await highlighterPromise;
-    const code = highlighter.codeToHtml(content, { lang: language });
+    const code = highlighter.codeToHtml(codeString, { lang: finalLanguage });
     return code;
   }
 
